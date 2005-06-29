0c840543855081e5c4495906f827e617198f82f0
hxr <>
2005-06-29 05:49:37 +0000

    Created a file to contain all the Vendor and Product IDs So far only the SPCA500Driver.m file uses it; the plan is to convert one file at a time.

---
+++ utilities/USB_VendorProductIDs.h
@@ -0,0 +1,59 @@
+/*
+ *  USB_VendorProductIDs.h
+ *  macam
+ *
+ *  Created by Harald Ruda on 6/29/05.
+ *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+#define VENDOR_SUNPLUS			0x04fc
+
+#define VENDOR_FINET_TECHNOLOGY 0x04fc
+#define	PRODUCT_PALMPIX_DC_85	0x7333		// SPCA500A
+
+#define VENDOR_MUSTEK			0x055f
+#define PRODUCT_GSMART_MINI2	0xc420
+
+#define VENDOR_SWEDA			0x0C45
+#define PRODUCT_SMARTCAM_VGAS	0x8008		// SPCA500A
+
+#define VENDOR_KODAK			0x040a
+#define	PRODUCT_EZ200			0x0300		// SPCA500A
+
+#define VENDOR_CREATIVE			0x041e
+#define	PRODUCT_PC_CAM_300		0x400a		// SPCA500A
+
+#define VENDOR_LOGITECH			0x046d
+#define	PRODUCT_CLICKSMART_510	0x0901		// SPCA500A
+
+#define VENDOR_VIEWQUEST		0x0733
+#define	PRODUCT_M318B			0x0402		// SPCA500A ?
+
+#define	VENDOR_MINTON			0x084d
+#define	PRODUCT_S_CAM_F5		0x0003		// SPCA500A
+
+#define	VENDOR_INTEL			0x8086
+#define	PRODUCT_POCKET_PC_CS630	0x0630		// SPCA500A
+#define	PRODUCT_PC_CAMERA_CS630	0x0630		// SPCA500A
+
+#define	VENDOR_D_LINK			0x084d
+#define	PRODUCT_DSC_350			0x0003		// SPCA500A
+//#define	PRODUCT_DSC_350F		0x0003		// SPCA500A
+
+#define	VENDOR_TRUST			0x084d
+#define	PRODUCT_FAMILYCAM_300	0x0003		// SPCA500A
+
+#define	VENDOR_AIPTEK			0x08ca
+//#define	PRODUCT_PENCAM_400		??
+#define	PRODUCT_POCKET_DV		0x0103		// SPCA500C
+#define	PRODUCT_EASY_CAM_DV		0x0103		// SPCA500C
+
+
+// do we need these?
+
+#define PRODUCT_SPCA500			0x500a
+#define PRODUCT_SPCA500B		0x500b
+
+
