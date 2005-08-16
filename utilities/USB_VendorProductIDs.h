c9b2f5dade16bd7e0a8d27a0562f43007ab5518f
hxr <>
2005-08-16 21:24:12 +0000

    Added a lot more vendor/product IDs to this file

--- utilities/USB_VendorProductIDs.h
+++ utilities/USB_VendorProductIDs.h
@@ -11,54 +11,84 @@
 #define VENDOR_SUNPLUS			0x04fc
 
 #define VENDOR_FINET_TECHNOLOGY 0x04fc
-#define	PRODUCT_PALMPIX_DC_85	0x7333		// SPCA500A
+#define	PRODUCT_PALMPIX_DC_85           0x7333		// SPCA500A
 
 #define VENDOR_MUSTEK			0x055f
-#define PRODUCT_GSMART_MINI2	0xc420
+#define PRODUCT_GSMART_MINI2            0xc420
 
 #define VENDOR_SONIX            0x0c45
 #define PRODUCT_DC31UC                  0x8000      // Sonix 2028
 #define PRODUCT_VIVICAM3350B            0x800A      // Sonix 2028
 
 #define VENDOR_SWEDA			0x0C45
-#define PRODUCT_SMARTCAM_VGAS	0x8008		// Probably *not* SPCA500A
+#define PRODUCT_SMARTCAM_VGAS           0x8008		// Probably *not* SPCA500A
 
 #define VENDOR_KODAK			0x040a
-#define	PRODUCT_EZ200			0x0300		// SPCA500A
+#define	PRODUCT_EZ200                   0x0300		// SPCA500A
 
-#define VENDOR_CREATIVE			0x041e
-#define	PRODUCT_PC_CAM_300		0x400a		// SPCA500A
+#define VENDOR_CREATIVE_LABS    0x041e
+#define	PRODUCT_PC_CAM_300              0x400a		// SPCA500A
+#define PRODUCT_WEBCAM_5                0x400c
+#define PRODUCT_WEBCAM_5_NEW            0x400d
+#define PRODUCT_WEBCAM_PRO_EX           0x4011
 
 #define VENDOR_LOGITECH			0x046d
+#define PRODUCT_QUICKCAM_PRO_3000       0x08b0
 #define PRODUCT_QUICKCAM_NOTEBOOK_PRO   0x08b1      // 740 (CCD)
+#define PRODUCT_QUICKCAM_PRO_4000       0x08b2
+#define PRODUCT_QUICKCAM_ZOOM           0x08b3
+#define PRODUCT_QUICKCAM_ZOOM_NEW       0x08b4
+#define PRODUCT_QUICKCAM_ORBIT          0x08b5
 #define	PRODUCT_CLICKSMART_510          0x0901		// SPCA500A
 
+#define VENDOR_PHILIPS          0x0471
+#define PRODUCT_VESTA                   0x0307
+#define PRODUCT_VESTA_PRO               0x0308
+#define PRODUCT_VESTA_SCAN              0x030c
+#define PRODUCT_TOUCAM_FUN              0x0310
+#define PRODUCT_TOUCAM_PRO              0x0311
+#define PRODUCT_TOUCAM_PRO_3D           0x0312
+#define PRODUCT_TOUCAM_XS               0x0313      // Philips PCVC720K/40 (ToUCam XS)
+
+#define VENDOR_SAMSUNG          0x055D
+#define PRODUCT_MPC_C10                 0x9000
+#define PRODUCT_MPC_C30                 0x9001
+
 #define VENDOR_VIEWQUEST		0x0733
-#define	PRODUCT_M318B			0x0402		// SPCA500A ?
+#define	PRODUCT_M318B                   0x0402		// SPCA500A ?
 
 #define	VENDOR_MINTON			0x084d
-#define	PRODUCT_S_CAM_F5		0x0003		// SPCA500A
+#define	PRODUCT_S_CAM_F5                0x0003		// SPCA500A
 
 #define	VENDOR_INTEL			0x8086
-#define	PRODUCT_POCKET_PC_CS630	0x0630		// SPCA500A
-#define	PRODUCT_PC_CAMERA_CS630	0x0630		// SPCA500A
+#define	PRODUCT_POCKET_PC_CS630         0x0630		// SPCA500A
+#define	PRODUCT_PC_CAMERA_CS630         0x0630		// SPCA500A
 
 #define	VENDOR_D_LINK			0x084d
-#define	PRODUCT_DSC_350			0x0003		// SPCA500A
+#define	PRODUCT_DSC_350                 0x0003		// SPCA500A
 //#define	PRODUCT_DSC_350F		0x0003		// SPCA500A
 
 #define	VENDOR_TRUST			0x084d
-#define	PRODUCT_FAMILYCAM_300	0x0003		// SPCA500A
+#define	PRODUCT_FAMILYCAM_300           0x0003		// SPCA500A
 
 #define	VENDOR_AIPTEK			0x08ca
 //#define	PRODUCT_PENCAM_400		??
-#define	PRODUCT_POCKET_DV		0x0103		// SPCA500C
-#define	PRODUCT_EASY_CAM_DV		0x0103		// SPCA500C
+#define	PRODUCT_POCKET_DV               0x0103		// SPCA500C
+#define	PRODUCT_EASY_CAM_DV             0x0103		// SPCA500C
 
+#define VENDOR_SOTEC            0x04cc
+#define PRODUCT_AFINA_EYE               0x8116
+
+#define VENDOR_AME_CO           0x06BE
+#define PRODUCT_AFINA_EYE               0x8116
+
+#define VENDOR_VISIONITE        0x0d81
+#define PRODUCT_VCS_UC300               0x1900
+#define PRODUCT_VCS_UM100               0x1910
 
 // do we need these?
 
-#define PRODUCT_SPCA500			0x500a
-#define PRODUCT_SPCA500B		0x500b
+#define PRODUCT_SPCA500                 0x500a
+#define PRODUCT_SPCA500B                0x500b
 
 
