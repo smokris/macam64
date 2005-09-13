f95a5698354f72de44cec53e9973c99a167965aa
hxr <>
2005-09-13 20:13:09 +0000

    Added a lot of IDs

--- utilities/USB_VendorProductIDs.h
+++ utilities/USB_VendorProductIDs.h
@@ -7,6 +7,7 @@
  *
  */
 
+// This file should contain, vendor, product, and a comment with the name of the driver that recognizes the camera
 
 #define VENDOR_SUNPLUS			0x04fc
 
@@ -21,7 +22,7 @@
 #define PRODUCT_VIVICAM3350B            0x800A      // Sonix 2028
 
 #define VENDOR_SWEDA			0x0C45
-#define PRODUCT_SMARTCAM_VGAS           0x8008		// Probably *not* SPCA500A
+#define PRODUCT_SMARTCAM_VGAS           0x8008		// Sonix 2028 something
 
 #define VENDOR_KODAK			0x040a
 #define	PRODUCT_EZ200                   0x0300		// SPCA500A
@@ -32,14 +33,38 @@
 #define PRODUCT_WEBCAM_5_NEW            0x400d
 #define PRODUCT_WEBCAM_PRO_EX           0x4011
 
+#define VENDOR_CONNECTIX		0x0478
+#define PRODUCT_QUICKCAM_VC             0x0001      // uses USS720 USB <-> parallel converter
+#define PRODUCT_QUICKCLIP               0x0002
+
 #define VENDOR_LOGITECH			0x046d
+#define PRODUCT_QUICKCAM_HOME           0x0801
+#define PRODUCT_QUICKCAM_PRO_BEIGE      0x0810      // uses USS720 USB <-> parallel converter
+#define PRODUCT_QUICKCAM_PRO_DARK       0xd001      // DIVIO NW801 controller
 #define PRODUCT_QUICKCAM_PRO_3000       0x08b0
 #define PRODUCT_QUICKCAM_NOTEBOOK_PRO   0x08b1      // 740 (CCD)
 #define PRODUCT_QUICKCAM_PRO_4000       0x08b2
 #define PRODUCT_QUICKCAM_ZOOM           0x08b3
 #define PRODUCT_QUICKCAM_ZOOM_NEW       0x08b4
 #define PRODUCT_QUICKCAM_ORBIT          0x08b5
+#define	PRODUCT_CLICKSMART_310          0x0900
 #define	PRODUCT_CLICKSMART_510          0x0901		// SPCA500A
+#define	PRODUCT_CLICKSMART_820_A        0x0903
+#define	PRODUCT_CLICKSMART_820_B        0x0905
+#define PRODUCT_QUICKCAM_EXPRESS_A      0x0840
+#define PRODUCT_QUICKCAM_WEB            0x0850
+#define PRODUCT_QUICKCAM_EXPRESS_B      0x0870
+#define PRODUCT_QUICKCAM_TRAVELER       0x0890
+#define PRODUCT_QUICKCAM_EXPRESS_C      0x0920
+#define PRODUCT_LABTEC_WEBCAM           0x0921
+#define PRODUCT_QUICKCAM_EXPRESS_D      0x0928
+#define PRODUCT_POCKET_CAMERA           0x0950
+#define PRODUCT_CLICKSMART_420          0x0960
+#define PRODUCT_POCKET_750              0x0970
+#define PRODUCT_QUICKCAM_IM             0x08a0
+#define PRODUCT_QUICKCAM_IM_MIC         0x08a1
+#define PRODUCT_LABTEC_WEBCAM_PRO       0x08a2
+#define PRODUCT_QUICKCAM_CHAT           0x08a3
 
 #define VENDOR_PHILIPS          0x0471
 #define PRODUCT_VESTA                   0x0307
@@ -49,6 +74,9 @@
 #define PRODUCT_TOUCAM_PRO              0x0311
 #define PRODUCT_TOUCAM_PRO_3D           0x0312
 #define PRODUCT_TOUCAM_XS               0x0313      // Philips PCVC720K/40 (ToUCam XS)
+
+#define VENDOR_STM              0x0553
+#define PRODUCT_STV0680                 0x0202
 
 #define VENDOR_SAMSUNG          0x055D
 #define PRODUCT_MPC_C10                 0x9000
@@ -75,6 +103,9 @@
 //#define	PRODUCT_PENCAM_400		??
 #define	PRODUCT_POCKET_DV               0x0103		// SPCA500C
 #define	PRODUCT_EASY_CAM_DV             0x0103		// SPCA500C
+
+#define VENDOR_ALARIS           0x0620
+#define PRODUCT_WEECAM                  0x0004      // QV208-02A - unknown
 
 #define VENDOR_SOTEC            0x04cc
 #define PRODUCT_AFINA_EYE               0x8116
