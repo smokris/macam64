b6cbc23467ce1c05e62158634cc9360e03f60285
hxr <>
2005-10-21 14:46:57 +0000

    Initial class structure for SPCA5XX drivers. This will probably change, and right now it does nothing anyway.

---
+++ cameras/SPCA5XX.h
@@ -0,0 +1,188 @@
+//
+//  SPCA5XX.h
+//
+//  macam - webcam app and QuickTime driver component
+//  SPCA5XX - driver for SPCA5XX-based cameras
+//
+//  Created by HXR on 9/19/05.
+//  Copyright (C) 2005 HXR (hxr@users.sourceforge.net). 
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA
+//
+
+#import <Cocoa/Cocoa.h>
+
+#include "MyCameraDriver.h"
+
+
+@interface SPCA5XX : MyCameraDriver 
+{
+
+}
+
+@end
+
+
+@interface SPCA500 : SPCA5XX 
+{
+    
+}
+
+@end
+
+
+@interface SPCA500A : SPCA500 
+{
+    
+}
+
+@end
+
+
+@interface SPCA500C : SPCA500A 
+{
+    
+}
+
+@end
+
+
+@interface SPCA501A : SPCA5XX 
+{
+    
+}
+
+@end
+
+
+@interface SPCA504A : SPCA5XX 
+{
+    
+}
+
+@end
+
+
+@interface SPCA504B : SPCA504A 
+{
+    
+}
+
+@end
+
+
+@interface SPCA504B_P3 : SPCA504B 
+{
+    
+}
+
+@end
+
+
+@interface SPCA505 : SPCA5XX 
+{
+    
+}
+
+@end
+
+
+@interface SPCA505B : SPCA505 
+{
+    
+}
+
+@end
+
+
+@interface SPCA506 : SPCA505 
+{
+    
+}
+
+@end
+
+
+@interface SPCA506A : SPCA506 
+{
+    
+}
+
+@end
+
+
+@interface SPCA508 : SPCA501A 
+{
+    
+}
+
+@end
+
+
+@interface SPCA508A : SPCA508 
+{
+    
+}
+
+@end
+
+
+@interface SPCA533 : SPCA504A 
+{
+    
+}
+
+@end
+
+
+@interface SPCA533A : SPCA533 
+{
+    
+}
+
+@end
+
+
+@interface SPCA536 : SPCA504A 
+{
+    
+}
+
+@end
+
+
+@interface SPCA536A : SPCA536 
+{
+    
+}
+
+@end
+
+
+@interface SPCA551A : SPCA5XX 
+{
+    
+}
+
+@end
+
+
+@interface SPCA561A : SPCA501A 
+{
+    
+}
+
+
+@end
