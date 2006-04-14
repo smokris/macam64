fcfb00c87df480c726ef8267162f8749ebe36de9
hxr <>
2006-04-14 05:05:27 +0000

    More decoding routines need to be exposed.

--- cameras/spca5xx_files/spcadecoder.h
+++ cameras/spca5xx_files/spcadecoder.h
@@ -10,10 +10,14 @@ int  spca50x_outpicture (struct spca50x_frame *myframe);
 
 void init_jpeg_decoder(struct usb_spca50x *spca50x );
 
+int  jpeg_decode422(struct spca50x_frame * myframe, int force_rgb);
+
 void init_sonix_decoder(struct usb_spca50x *spca50x);
 
 void init_pixart_decoder(struct usb_spca50x * spca50x);
 int  pixart_decompress(struct spca50x_frame * myframe);
+
+void tv8532_preprocess(struct spca50x_frame * myframe);
 
 void decode_spca561(unsigned char *inbuf, unsigned char *outbuf, int width, int height);
 
