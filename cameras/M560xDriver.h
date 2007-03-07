20b309602b980f7d1b500c4ac714d2659ed21149
hxr <>
2007-03-07 02:13:37 +0000

    It's pretty close to a start.

--- cameras/M560xDriver.h
+++ cameras/M560xDriver.h
@@ -1,3 +1,41 @@
+//
+//  M560xDriver.h
+//  macam
+//
+//  Created by Harald on 3/1/07.
+//  Copyright 2007 hxr. All rights reserved.
+//
+
+
+#import <GenericDriver.h>
+
+
+@interface M560xDriver : GenericDriver 
+{
+    // Add any data structure that you need to keep around
+    // i.e. decoding buffers, decoding structures etc
+}
+
++ (NSArray *) cameraUsbDescriptions;
+
+- (id) initWithCentral: (id) c;
+
+- (BOOL) supportsResolution: (CameraResolution) res fps: (short) rate;
+- (CameraResolution) defaultResolutionAndRate: (short *) rate;
+
+- (UInt8) getGrabbingPipe;
+- (BOOL) setGrabInterfacePipe;
+- (void) setIsocFrameFunctions;
+
+- (BOOL) startupGrabStream;
+- (void) shutdownGrabStream;
+
+- (BOOL) decodeBuffer: (GenericChunkBuffer *) buffer;
+
+@end
+
+
+
 // M560x
 // see http://gkall.hobby.nl/m560x.html
 
@@ -6,6 +44,8 @@
 
 // Creative Live! Cam Voice uses the ALi M5603C [0x041E/0x4045]
 
+/*
+ 
 (USB 2.0 version probably)
 
 T:  Bus=01 Lev=01 Prnt=01 Port=05 Cnt=01 Dev#=  3 Spd=480 MxCh= 0
@@ -45,3 +85,27 @@ E:  Ad=82(I) Atr=03(Int.) MxPS=  16 Ivl=4ms
 I:  If#= 0 Alt= 3 #EPs= 2 Cls=ff(vend.) Sub=ff Prot=ff Driver=(none)
 E:  Ad=81(I) Atr=01(Isoc) MxPS= 768 Ivl=1ms
 E:  Ad=82(I) Atr=03(Int.) MxPS=  16 Ivl=4ms
+
+
+
+Ali m560x Linux Driver
+======================
+
+The project is currently in a very early stage. For updated info,
+please refer to the web site: http://m560x.x3ng.com
+
+Supported chipsets and Webcams
+------------------------------
+
+* M5602:
+    * Asus A6K Laptop w/ 1.3 Megapixel WebCam
+    * Asus W5A Laptop w/ 1.3 Megapixel WebCam
+    * Clevo M550G 300k Webcam integrated into the lid
+    * Zepto Znote 2114W Laptop w/ Webcam
+
+* M5603c:
+    * Q-TEC Webcam 300 USB 2.0
+    * Trust 360 USB 2.0 SpaceCam
+
+
+*/
---
+++ cameras/M560xDriver.m
@@ -0,0 +1,213 @@
+//
+//  M560xDriver.m
+//  macam
+//
+//  Created by Harald on 3/2/07.
+//  Copyright 2007 hxr. All rights reserved.
+//
+
+
+#import "M560xDriver.h"
+
+#include "USB_VendorProductIDs.h"
+
+
+@implementation M560xDriver
+
+//
+// Specify which Vendor and Product IDs this driver will work for
+// Add these to the USB_VendorProductIDs.h file
+//
++ (NSArray *) cameraUsbDescriptions 
+{
+    return [NSArray arrayWithObjects:
+        
+
+        [NSDictionary dictionaryWithObjectsAndKeys:
+            [NSNumber numberWithUnsignedShort:0x4055], @"idProduct",
+            [NSNumber numberWithUnsignedShort:VENDOR_CREATIVE_LABS], @"idVendor",
+            @"Creative Live! Cam Video IM Pro", @"name", NULL], 
+        
+        // More entries can easily be added for more cameras
+        
+        NULL];
+}
+
+//
+// Initialize the driver
+//
+- (id) initWithCentral: (id) c 
+{
+	self = [super initWithCentral:c];
+	if (self == NULL) 
+        return NULL;
+    
+    bayerConverter = [[BayerConverter alloc] init];
+	if (bayerConverter == NULL) 
+        return NULL;
+    
+    // Allocate memory
+    // Initialize variable and other structures
+    
+	return self;
+}
+
+//
+// Provide feedback about which resolutions and rates are supported
+//
+- (BOOL) supportsResolution: (CameraResolution) res fps: (short) rate 
+{
+    switch (res) 
+    {
+        case ResolutionVGA:
+            if (rate > 30) 
+                return NO;
+            return YES;
+            break;
+            
+        default: 
+            return NO;
+    }
+}
+
+//
+// Return the default resolution and rate
+//
+- (CameraResolution) defaultResolutionAndRate: (short *) rate
+{
+	if (rate) 
+        *rate = 15;
+    
+	return ResolutionVGA;
+}
+
+//
+// Returns the pipe used for grabbing
+//
+- (UInt8) getGrabbingPipe
+{
+    return 1;
+}
+
+//
+// Put in the alt-interface with the highest bandwidth (instead of 8)
+// This attempts to provide the highest bandwidth
+//
+- (BOOL) setGrabInterfacePipe
+{
+    return [self usbMaximizeBandwidth:[self getGrabbingPipe]  suggestedAltInterface:-1  numAltInterfaces:3];
+}
+
+//
+// This is an example that will have to be tailored to the specific camera or chip
+// Scan the frame and return the results
+//
+IsocFrameResult  m560xIsocFrameScanner(IOUSBIsocFrame * frame, UInt8 * buffer, 
+                                         UInt32 * dataStart, UInt32 * dataLength, 
+                                         UInt32 * tailStart, UInt32 * tailLength)
+{
+    int position, frameLength = frame->frActCount;
+    
+    *dataStart = 0;
+    *dataLength = frameLength;
+    
+    *tailStart = frameLength;
+    *tailLength = 0;
+    
+    if (frameLength < 2) 
+    {
+        *dataLength = 0;
+        
+#if REALLY_VERBOSE
+        printf("Invalid chunk!\n");
+#endif
+        return invalidFrame;
+    }
+    
+#if REALLY_VERBOSE
+    printf("buffer[0] = 0x%02x (length = %d) 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x\n", buffer[0], frameLength, buffer[1], buffer[2], buffer[3], buffer[4], buffer[5]);
+#endif
+    
+    for (position = 0; position < frameLength - 2; position++) 
+    {
+        if ((buffer[position+0] == 0xFF) && 
+            (buffer[position+1] == 0xFF)) 
+        {
+#if REALLY_VERBOSE
+            printf("New chunk! (position = %d, then 0x%02x 0x%02x 0x%02x)\n", position, buffer[position+2], buffer[position+3], buffer[position+4]);
+#endif
+            if (position > 0) 
+            {
+                *tailStart = 0;
+                *tailLength = position;
+            }
+            
+            *dataStart = position;
+            *dataLength = frameLength - position;
+            
+            return newChunkFrame;
+        }
+    }
+    
+    return validFrame;
+}
+
+//
+// These are the C functions to be used for scanning the frames
+//
+- (void) setIsocFrameFunctions
+{
+    grabContext.isocFrameScanner = m560xIsocFrameScanner;
+    grabContext.isocDataCopier = genericIsocDataCopier;
+}
+
+//
+// This is the key method that starts up the stream
+//
+- (BOOL) startupGrabStream 
+{
+    CameraError error = CameraErrorOK;
+    
+    // started when the alt interface was set
+    
+    return error == CameraErrorOK;
+}
+
+//
+// The key routine for shutting down the stream
+//
+- (void) shutdownGrabStream 
+{
+    [self usbSetAltInterfaceTo:0 testPipe:[self getGrabbingPipe]]; // Must set alt interface to normal
+}
+
+//
+// This is the method that takes the raw chunk data and turns it into an image
+//
+- (BOOL) decodeBuffer: (GenericChunkBuffer *) buffer
+{
+    BOOL ok = YES;
+	short rawWidth  = [self width];
+	short rawHeight = [self height];
+    
+	// Decode the bytes
+    
+    //  Much decoding to be done here
+    
+    // Turn the Bayer data into an RGB image
+    
+    [bayerConverter setSourceFormat:3]; // This is probably different
+    [bayerConverter setSourceWidth:rawWidth height:rawHeight];
+    [bayerConverter setDestinationWidth:rawWidth height:rawHeight];
+    [bayerConverter convertFromSrc:buffer->buffer
+                            toDest:nextImageBuffer
+                       srcRowBytes:rawWidth
+                       dstRowBytes:nextImageBufferRowBytes
+                            dstBPP:nextImageBufferBPP
+                              flip:hFlip
+                         rotate180:NO]; // This might be different too
+    
+    return ok;
+}
+
+@end
