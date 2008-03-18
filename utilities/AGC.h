d99f8aa09c2b9e23af6a48977272a5170e5c5343
hxr <>
2008-03-18 16:14:56 +0000

    Add a class for AGC, though not used yet.

---
+++ utilities/AGC.h
@@ -0,0 +1,33 @@
+//
+//  AGC.h
+//  macam
+//
+//  Created by Harald on 3/16/08.
+//  Copyright 2008 hxr. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+
+//
+// Algorithm for Automatic Gain Control
+//
+// Some cameras do not have AGC built-in, but require driver-level support. 
+//
+
+//
+// connect with Histogram, or use other data?
+// 
+// update often, but not necessarily every frame
+// 
+// keep track of recent changes, to understand effects
+//
+
+
+@interface AGC : NSObject 
+{
+
+}
+
+@end
---
+++ utilities/AGC.m
@@ -0,0 +1,14 @@
+//
+//  AGC.m
+//  macam
+//
+//  Created by Harald on 3/16/08.
+//  Copyright 2008 hxr. All rights reserved.
+//
+
+#import "AGC.h"
+
+
+@implementation AGC
+
+@end
