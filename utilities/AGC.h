4c65afdce9cfe072b80dfa6582790fa2fd737bf2
hxr <>
2008-04-08 19:35:04 +0000

    Now we have working version of this class.

--- utilities/AGC.h
+++ utilities/AGC.h
@@ -11,11 +11,33 @@
 #import <Histogram.h>
 
 
+typedef enum AGCMode
+{
+    agcNone,
+    agcProvidedAverage,
+    agcHistogram,
+    
+} AGCMode;
+
+
+typedef enum AGCEffect 
+{
+    agcAffectNone,
+    agcAffectGain,
+    agcAffectOffset,
+    agcAffectShutter,
+    agcAffectBrightness,
+    
+} AGCEffect;
+
+
 //
 // Algorithm for Automatic Gain Control
 //
 // Some cameras do not have AGC built-in, but require driver-level support. 
 //
+
+@class MyCameraDriver;
 
 //
 // connect with Histogram, or use other data?
@@ -28,12 +50,37 @@
 
 @interface AGC : NSObject 
 {
-    BOOL softwareAGCon;
+    MyCameraDriver * driver;
     
+    AGCMode mode;
+    NSArray * list;
+    
+    int effectCount;
+    AGCEffect effect1;
+    AGCEffect effect2;
+    AGCEffect effect3;   
+    
+    int target;
+    int delta;
+    
+    struct GenericFrameInfo * frameInfo;
+    
+    int fastUpdate;
+    int slowUpdate;
+    int updateInterval;
+    struct timeval tvLastUpdate;
 }
 
+- (id) initWithDriver:(MyCameraDriver *) driver;
+
+- (void) setMode:(AGCMode) newMode;
+- (void) setEffects:(NSArray *) array;
+- (void) setFrameInfo:(struct GenericFrameInfo *) frameInfo;
 
 - (BOOL) update:(Histogram *) histogram;
+- (BOOL) updateProvided;
+- (BOOL) updateHistogram:(Histogram *) histogram;
 
+- (BOOL) updateVersion1:(int) middle;
 
 @end
