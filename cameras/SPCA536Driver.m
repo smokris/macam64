6fd0a8426c8862158453bb9c8880bf852ac8c127
hxr <>
2008-05-28 21:19:18 +0000

    Added frame scanner.

--- cameras/SPCA536Driver.m
+++ cameras/SPCA536Driver.m
@@ -34,6 +34,64 @@
 	return self;
 }
 
+//
+// Scan the frame and return the results
+//
+IsocFrameResult  spca536IsocFrameScanner(IOUSBIsocFrame * frame, UInt8 * buffer, 
+                                         UInt32 * dataStart, UInt32 * dataLength, 
+                                         UInt32 * tailStart, UInt32 * tailLength, 
+                                         GenericFrameInfo * frameInfo)
+{
+    int frameLength = frame->frActCount;
+    
+    *dataStart = 0;
+    *dataLength = frameLength;
+    
+    *tailStart = frameLength;
+    *tailLength = 0;
+    
+    
+    if (frameLength < 1) 
+    {
+        *dataLength = 0;
+        
+#if REALLY_VERBOSE
+//        printf("Invalid packet.\n");
+#endif
+        return invalidFrame;
+    }
+    
+#if REALLY_VERBOSE
+//    printf("buffer[0] = 0x%02x (length = %d) 0x%02x ... [129] = 0x%02x ... 0x%02x 0x%02x 0x%02x 0x%02x\n", 
+//           buffer[0], frameLength, buffer[1], buffer[129], buffer[frameLength-4], buffer[frameLength-3], buffer[frameLength-2], buffer[frameLength-1]);
+#endif
+    
+    if (buffer[0] == SPCA50X_SEQUENCE_DROP) // start a new image
+    {
+#if REALLY_VERBOSE
+//        printf("New image start!\n");
+#endif
+        *dataStart = 4; //  SPCA536_OFFSET_DATA;
+        *dataLength = frameLength - *dataStart;
+        
+        return newChunkFrame;
+    }
+    
+    *dataStart = 2;
+    *dataLength = frameLength - *dataStart;
+    
+    return validFrame;
+}
+
+//
+// These are the C functions to be used for scanning the frames
+//
+- (void) setIsocFrameFunctions
+{
+    grabContext.isocFrameScanner = spca536IsocFrameScanner;
+    grabContext.isocDataCopier = genericIsocDataCopier;
+}
+
 @end
 
 
